b7c72a7c007e88bd96bf4446d74fb5301c136194
DESKTOP-3DMF6CO\zhuny <zhuny936772@gmail.com>
2025-05-09 01:09:52 +0900

    connect to obs

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from PySide6 import QtWidgets
 from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QLineEdit, QLabel, QTextEdit, QFormLayout, QVBoxLayout, QWidget, QGroupBox, QPushButton
+import obsws_python as obs
+from obsws_python.error import OBSSDKError, OBSSDKRequestError
 
 
 class InputPair:
@@ -77,21 +79,62 @@ class MyTimer(QTimer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.socket = None
+        self.socket: obs.ReqClient | None = None
         self.timeout.connect(self.handle)
 
     def start_timer(self):
-        if self.connect_to_obs():
+        parent = self.parent()
+        if self._connect_to_obs(**parent.get_input_value()):
+            parent.push_log("인식 시작")
             self.start(1000)
+        elif self.socket is not None:
+            self.socket.disconnect()
+            self.socket = None
+            parent.push_log("연결 해제 - 재시도 필요")
 
-    def connect_to_obs(self):
-        return self._connect_to_obs(**self.parent().get_input_value())
-
-    def _connect_to_obs(self, websocket_port, **kwargs):
+    def _connect_to_obs(self,
+                        websocket_port, websocket_password,
+                        text_layer, switch_layer,
+                        **kwargs):
         parent: MyWidget = self.parent()
-
         parent.push_log(f"연결 시도 (포트: {websocket_port})")
-        return False
+        if self._check_connect_socket(websocket_port, websocket_password):
+            parent.push_log("연결 성공")
+        else:
+            parent.push_log("연결 실패 - 연결 설정 확인 필요")
+            return False
+
+        if self._check_obs_source(text_layer):
+            parent.push_log(f"OBS 소스({text_layer}) 확인")
+        else:
+            parent.push_log(f"OBS 소스({text_layer}) 없음 - '클리어 수 텍스트 레이어' 확인 필요")
+            return False
+
+        if self._check_obs_source(switch_layer):
+            parent.push_log(f"OBS 소스({switch_layer}) 확인")
+        else:
+            parent.push_log(f"OBS 소스({switch_layer}) 없음 - '스위치 화면 레이어' 확인 필요")
+            return False
+
+        return True
+
+    def _check_connect_socket(self, websocket_port, websocket_password):
+        try:
+            self.socket = obs.ReqClient(
+                host="localhost",
+                port=websocket_port,
+                password=websocket_password
+            )
+            return True
+        except OBSSDKError:
+            return False
+
+    def _check_obs_source(self, source_name):
+        try:
+            self.socket.get_source_active(source_name)
+            return True
+        except OBSSDKRequestError:
+            return False
 
     def handle(self):
         pass
