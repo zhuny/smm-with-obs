c4d919094f965584a021d164e82f3335d1f1eafb
DESKTOP-3DMF6CO\zhuny <zhuny936772@gmail.com>
2025-06-27 20:56:30 +0900

    set comfortable number input

--- app/widgets/edit.py
+++ app/widgets/edit.py
@@ -1,9 +1,57 @@
-from PySide6.QtGui import QIntValidator
-from PySide6.QtWidgets import QLabel, QLineEdit
+from PySide6.QtWidgets import QLabel, QLineEdit, QSpinBox
+
+
+class EditorWrapper:
+    def __init__(self, child):
+        self.child = child
+
+    def bind(self, handler):
+        raise NotImplementedError(self)
+
+    def set_value(self, value):
+        raise NotImplementedError(self)
+
+    def get_value(self):
+        raise NotImplementedError(self)
+
+    def hide_input(self):
+        self.child.setEchoMode(QLineEdit.EchoMode.Password)
+
+
+class NumberEditor(EditorWrapper):
+    def __init__(self):
+        box = QSpinBox()
+        box.setMaximum(1_000_000)
+        box.setMinimum(0)
+
+        super().__init__(box)
+
+    def bind(self, handler):
+        self.child.textChanged.connect(handler)
+
+    def set_value(self, value):
+        self.child.setValue(int(value))
+
+    def get_value(self):
+        return self.child.value()
+
+
+class TextEditor(EditorWrapper):
+    def __init__(self):
+        super().__init__(QLineEdit())
+
+    def bind(self, handler):
+        self.child.textEdited.connect(handler)
+
+    def get_value(self):
+        return self.child.text()
+
+    def set_value(self, value):
+        self.child.setText(str(value))
 
 
 class InputPair:
-    def __init__(self, name, title, *, is_number=False, is_password=False):
+    def __init__(self, name, title, *, default=None, is_number=False, is_password=False):
         self.name = name
         self.title = title
         self.is_number = is_number
@@ -13,23 +61,28 @@ class InputPair:
         self.parent: MyWidget | None = None
 
         self.label = QLabel(self.title)
-        self.edit = QLineEdit()
-        self.edit.textEdited.connect(self.on_edited)
+        if self.is_number:
+            self.edit_wrap = NumberEditor()
+        else:
+            self.edit_wrap = TextEditor()
 
-        if is_number:
-            self.edit.setValidator(QIntValidator())
+        self.edit_wrap.bind(self.on_edited)
+
         if is_password:
-            self.edit.setEchoMode(QLineEdit.EchoMode.Password)
+            self.edit_wrap.hide_input()
+        if default:
+            self.update_value(default)
 
     @property
     def value(self):
-        text_or_number = self.edit.text()
-        if self.is_number:
-            text_or_number = int(text_or_number or 0)
-        return text_or_number
+        return self.edit_wrap.get_value()
+
+    @property
+    def edit(self):
+        return self.edit_wrap.child
 
     def update_value(self, value):
-        self.edit.setText(str(value))
+        self.edit_wrap.set_value(value)
 
     def on_edited(self):
         self.parent.update_value(self.name, self.value)
